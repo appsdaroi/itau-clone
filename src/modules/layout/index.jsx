6c46763c631fe7e3efcebbeb36dad3af82d1a8ae
Felipe Boni <53157965+felipeboni@users.noreply.github.com>
2023-04-08 09:47:39 -0300

    fix pwa

--- src/modules/layout/index.jsx
+++ src/modules/layout/index.jsx
@@ -3,21 +3,20 @@ import { useRouter } from "next/router";
 
 const Layout = ({ children }) => {
   const router = useRouter();
-
   return (
     <div className="flex flex-col h-screen">
       {children}
 
       <nav className="fixed bottom-0 left-0 right-0 grid h-[4.5rem] grid-cols-5 gap-3 px-2 pt-2 pb-3 bg-white border-t">
         {router.pathname == "/" ? (
-          <Link prefetch={false} href="/" className="flex justify-center items-top">
+          <Link shallow href="/" className="flex justify-center items-top">
             <div className="flex items-center justify-center w-10 h-10 rounded-lg bg-primary">
               <i className="icon text-2xl before:content-['\e995'] text-white" />
             </div>
           </Link>
         ) : (
           <Link
-            prefetch={false}
+            shallow
             href="/"
             className="flex flex-col items-center justify-start gap-1.5"
           >
@@ -28,7 +27,7 @@ const Layout = ({ children }) => {
 
         {router.pathname == "/balance" ? (
           <Link
-            prefetch={false}
+            shallow
             href="/balance"
             className="flex justify-center items-top"
           >
@@ -38,7 +37,7 @@ const Layout = ({ children }) => {
           </Link>
         ) : (
           <Link
-            prefetch={false}
+            shallow
             href="/balance"
             className="flex flex-col items-center justify-start gap-1.5"
           >
